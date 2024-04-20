23d8cec854a40be2d1da3f98a19fc0bcefa1eff5
Marian Toma <164779986+MarianToma0@users.noreply.github.com>
2024-04-20 21:40:36 +0300

    Add files via upload

---
+++ Inchirieri/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Inchirieri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(263, 52);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(439, 225);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(402, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Lista masini pentru inchiriat";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(953, 510);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Inchirieri/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inchirieri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inițializare și afișare mașini
+            AfiseazaMasini();
+        }
+
+        private void AfiseazaMasini()
+        {
+            // Lista de mașini
+            List<Masina> masini = new List<Masina>
+            {
+                new Masina { Nume = "Dacia Logan" },
+                new Masina { Nume = "Volkswagen Golf" },
+                new Masina { Nume = "Ford Focus" }
+            };
+
+            List<int> ani = new List<int> { 2020, 2019, 2021 };
+
+            List<decimal> preturi = new List<decimal> { 100, 150, 120 };
+
+            List<bool> inchiriate = new List<bool> { false, true, false };
+
+            // Afisare mașini în TextBox-uri
+            for (int i = 0; i < masini.Count; i++)
+            {
+                TextBox textBoxNume = new TextBox
+                {
+                    Multiline = true,
+                    ReadOnly = true,
+                    Text = $"Nume: {masini[i].Nume}"
+                };
+
+                TextBox textBoxAn = new TextBox
+                {
+                    Multiline = true,
+                    ReadOnly = true,
+                    Text = $"An: {ani[i]}"
+                };
+
+                TextBox textBoxPret = new TextBox
+                {
+                    Multiline = true,
+                    ReadOnly = true,
+                    Text = $"Preț: {preturi[i]} lei/zi"
+                };
+
+                TextBox textBoxInchiriata = new TextBox
+                {
+                    Multiline = true,
+                    ReadOnly = true,
+                    Text = $"Stare: {(inchiriate[i] ? "Închiriată" : "Disponibilă")}"
+                };
+
+                // Adăugare TextBox-uri la formular
+                flowLayoutPanel1.Controls.Add(textBoxNume);
+                flowLayoutPanel1.Controls.Add(textBoxAn);
+                flowLayoutPanel1.Controls.Add(textBoxPret);
+                flowLayoutPanel1.Controls.Add(textBoxInchiriata);
+            }
+        }
+        public class Masina
+        {
+            public string Nume { get; set; }
+            public int An { get; set; }
+            public decimal Pret { get; set; }
+            public bool Inchiriata { get; set; }
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+
